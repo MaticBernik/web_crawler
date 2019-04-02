ed5632f16ae89be3655b51fcee5bf5cb457bcfe7
Robert Tovornik <robert.tovornik@gmail.com>
2019-04-02 21:05:36 +0200

    added-file-links

--- crawler/page_parser.py
+++ crawler/page_parser.py
@@ -6,7 +6,16 @@ import page_fetcher
 import re
 from url_normalize import url_normalize
 
-def parse_page_html(base_url, page_html):
+def fetch_file_content(file_url):
+    """ If successful returns a file of type bytes."""
+    r = requests.get(file_url)
+
+    if r.status_code == 200:
+        return r.content
+    else:
+        return None
+
+def parse_page_html(domain_url, page_url, page_html):
 
     soup = BeautifulSoup(page_html, "lxml")
 
@@ -15,32 +24,53 @@ def parse_page_html(base_url, page_html):
     image_urls = [ image['src'] for image in image_tags if image.get('src') is not None ]
     
     # find all links
+    ## TODO: HREF - JS onclick
     ## TODO: IGNORE OUTGOING LINKS (LEAVING THE BASE URL) - DIFFERENCE BETWEEN BASE AND DOMAIN URL ???
-    link_hrefs = soup.find_all('a', href=True)
+    # link_hrefs = soup.find_all('a', href=True)
     link_urls = []
+    file_urls = []
+    link_hrefs = soup.find_all('a', href=True)
     for link in link_hrefs:
         href = link.get('href')
-        # build dynamic hrefs
+
+        # build dynamic links
         if href[:4] != "http":
-            # href = url_normalize(base_url+href)
-            href = base_url + href[1:]
+
+            if href[0] == '/':
+                href = domain_url + href[1:]
+            else:
+                href = domain_url + href
         
-        link_urls.append(href)
+        if href.endswith('.pdf') or  href.endswith('.doc') or href.endswith('.docx') or href.endswith('.ppt') or href.endswith('.pptx'):
+            file_urls.append(href)
+        else:
+            link_urls.append(href)
 
-    ## TODO: HREF - JS onclick
-    ## TODO: FILE LINKS (pdf, docx,..)
 
-    
-    return link_hrefs, image_urls, None
+    print("IMAGES:")
+    for img in image_urls:
+        print(img)
+
+    print("FILES:")
+    for f in file_urls:
+        print(f)
+
+    print("LINKS:")
+    for link in link_urls:
+        print(link)
+
+
+    return link_hrefs, image_urls, file_urls
 
 
 
 def main():
     # image_site = "https://unsplash.com/search/photos/wallpaper"
-    image_site = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick"
+    # image_site = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick"
+    image_site = "http://www.e-prostor.gov.si/"
     resp_status, image_html = page_fetcher.fetch_page(image_site)
-    href_links, images, documents = parse_page_html("https://unsplash.com/", image_html)
+    href_links, images, documents = parse_page_html("https://www.e-prostor.gov.si/", image_site, image_html)
     print("complete")
-
+    
 if __name__ == "__main__":
     main()
--- crawler/tempCodeRunnerFile.py
+++ crawler/tempCodeRunnerFile.py
@@ -1,15 +1,4 @@
-xmlDict = {}
-
-r = requests.get("http://www.site.co.uk/sitemap.xml")
-xml = r.text
-
-soup = BeautifulSoup(xml)
-sitemapTags = soup.find_all("sitemap")
-
-print "The number of sitemaps are {0}".format(len(sitemapTags))
-
-for sitemap in sitemapTags:
-    xmlDict[sitemap.findNext("loc").text] = sitemap.findNext("lastmod").text
-
-print xmlDict
-
+file_urls=['a']
+file_urls[0] = "http://www.google.si/"
+print(fetch_file_content(file_urls[0]))
+print(type(file_urls[0]))
